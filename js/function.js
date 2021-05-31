3ed058d19b003575c386a862078a736a99fee484
Vojtech Varecha <viotal.varecha@outlook.com>
2021-05-31 14:24:45 +0200

    init commit

---
+++ js/function.js
@@ -0,0 +1,139 @@
+/*@licstart  The following is the entire license notice for the 
+JavaScript code in this page.
+
+Copyright (C) 2021 Vojtech Varecha
+Copyright (C) 2021 Jan Krehlik
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this page.
+*/
+
+//aliases
+function product(input){
+    return multiply(input);
+}
+//end of aliases 
+
+/**
+ * converts sth to number
+ * @param {String|Number} input
+ * @returns {Number}
+ */
+function toNumber(input){
+    let result = new Number;
+    if(typeof input == "number"){
+        result = input;
+    }else if(typeof input == "string"){
+        
+    }
+}
+
+/**
+* just testing function
+* @param {Array} arrayin array of input 
+* @returns {Array} =arrayin
+*/
+
+function test(arrayin){
+    return arrayin;
+}
+
+function flatDeep(arr) {//should be remade
+    d = Infinity;
+    return d > 0 ? arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatDeep(val, d - 1) : val), [])
+                 : arr.slice();
+ };
+
+/**
+ * sums all the number in Array (array in array will by sumed to)
+ * @param {Array|Number} input
+ * @returns {Number} sum of input
+ */
+function sum(input){
+    let result = 0;
+    input = flatDeep(input);
+    input.forEach(element => {
+        result = Number(result) + Number(element);
+    })
+    if(typeof input == "number"){
+        result = input;
+    }
+    return result;
+}
+
+/**
+ * substract all the number in Array from the first one (array in array will by used to to)
+ * @param {Array|Number} input
+ * @returns {Number}
+ */
+function substract(input){
+    let result = 0;
+    if(typeof input[0] !== undefined){
+        result = input[0];
+        input.shift();
+    }
+    if(typeof input == "object"){
+        input = flatDeep(input);
+        input.forEach(element => {
+            result = Number(result) - Number(element);
+        });
+    }else if(typeof input == "number"){
+        result = input;
+    }
+    return result;
+}
+
+/**
+ * multiply all the number in Array (array in array will by multiplyed to)
+ * @param {Array|Number} input
+ * @returns {Number} sum of input
+ */
+function multiply(input){
+    let result = 1;
+    if(typeof input == "object"){
+        input = flatDeep(input);
+        input.forEach(element => {
+            result = Number(result) * Number(element);
+        })
+    }
+    if(typeof input == "number"){
+        result = input;
+    }
+    return result;
+}
+
+/**
+ * divide all the number in Array from the first one (numbers in array will by used to to divide)
+ * @param {Array|Number} input
+ * @returns {Number}
+ */
+ function divide(input){
+    let result = 0;
+    if(typeof input == "object"){
+        input = flatDeep(input);
+        if(typeof input[0] !== undefined){
+            result = input[0];
+            input.shift();
+        }
+        input.forEach(element => {
+            result = Number(result) / Number(element);
+        });
+    }else if(typeof input == "number"){
+        result = input;
+    }
+    return result;
+} 
---
+++ js/render.js
@@ -0,0 +1,100 @@
+/*@licstart  The following is the entire license notice for the 
+JavaScript code in this page.
+
+Copyright (C) 2021 Vojtech Varecha
+Copyright (C) 2021 Jan Krehlik
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this page.
+*/
+
+/**
+ * Generates an empty spreadsheet element
+ * @param {Number} width Spreadsheet width...
+ * @param {Number} height Spreadsheet hight...
+ * @returns {Element} Pregenerated spreadsheet of defined size
+ */
+function generateSpreadsheet(width, height) {
+    // TODO var en_alphabeth = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+    var spreadsheet = document.createElement("div");
+    spreadsheet.className = "spreadsheet";
+    spreadsheet.id = "spreadsheet";
+
+    for (var row_number = 0; row_number <= height; row_number++) {
+        column_number = 0;
+
+        var th = document.createElement('div');
+        th.className = "spreadsheet-header";
+        th.id = "th"+row_number;
+
+        if (row_number > 0) {
+            th.innerHTML = row_number;
+        }else{
+            th.innerHTML = "*";
+            th.style.zIndex = "9999";
+        }
+
+        th.style.gridColumnStart=column_number+1;
+        th.style.gridRowStart=row_number+1;
+        spreadsheet.appendChild(th);
+
+        for (var column_number = 1; column_number <= width; column_number++) {
+            
+            // if it's the first row insert alphabetical header
+            // TODO
+            if (row_number == 0) {
+                var th = document.createElement('div');
+                th.innerHTML = column_number;
+                th.className = "spreadsheet-header";
+                th.id = "th"+column_number;
+
+                th.style.gridColumnStart=column_number+1;
+                th.style.gridRowStart=row_number+1;
+                spreadsheet.appendChild(th);
+            }else{
+                var cell = document.createElement('div');
+                cell.className = "spreadsheet-cell";
+                cell.id = "cell"+column_number+"-"+row_number;
+                // cell.innerHTML = "-"; //For testing
+
+                cell.style.gridColumnStart=column_number+1;
+                cell.style.gridRowStart=row_number+1;
+                spreadsheet.appendChild(cell);
+            }
+        }
+    }
+    return spreadsheet;
+}
+
+/**
+ * Appends empty spreadsheet to 
+ * @param {Element} element The element you wat to append the spreadsheet to
+ * @param {Number} width Spreadsheet width...
+ * @param {Number} height Spreadsheet hight...
+ * @returns {*} Appends pregenerated spreadsheet of defined size to selected element.
+ */
+function appendSpreadsheet(element,width,height){
+    // attempt to clean input type?
+    // element = Element(element);
+    element.appendChild(generateSpreadsheet(width,height));
+}
+
+appendSpreadsheet(document.getElementById("spreadsheet-wrapper"),50,500);
+/*
+console.log(document.getElementsByName(body));
+*/
